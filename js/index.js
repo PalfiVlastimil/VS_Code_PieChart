76fd36ed258ffd5ef5040abba25b9947dc7d3e04
Vlastimil Pálfi <u18.421@pslib.cloud>
2020-04-12 14:33:07 +0200

    repairing table & position

--- js/index.js
+++ js/index.js
@@ -19,28 +19,35 @@ const init = function(){
        array.push(average);
 
     }
-
     average = 360 / average // 60 => průměr pro hodnoty
     
 
     let tbody = document.createElement("tbody");
+    let tr1 = document.createElement("tr");
+    let textColor = document.createElement("td");
+    let textAngle = document.createElement("td");
+    textColor.innerText = "Hodnota & Barva";
+    textAngle.innerText = "Úhel";
+    tbody.append(tr1);
+    tr1.append(textColor);
+    tr1.append(textAngle);
+
     let trueAngles = 0;
     let angles = 0;
     let sector;
-    for (let x = 0; x < array.length; x++) {
+    for (let x = 0; x < array.length; x++) { //prochátí pole hodnot pro výseče a zároveň pravé hodnoty výseče
         let className = "sektor"
         let cont = document.getElementById("pieContainer")
         let div = document.createElement("div")
-        angles = average * array[x];
-        trueAngles = average * data[x];
+        angles = average * array[x]; //úhly výsečů
+        trueAngles = average * data[x]; //úhly pravých hodnot
         console.log(trueAngles)
-        colors = getRandomColor()
+       
+        colors = getRandomColor() //náhodné barvy (prochází obě pole)
 
         div.style.zIndex = -x;
         
         cont.append(div);
-
-        
         div.classList.add(className + x, "pie");
         
         sector = new Sektor(".sektor"+ x,{
@@ -48,21 +55,24 @@ const init = function(){
             circleColor: "none",
             sectorColor: colors
         })
-        
+
         trueAngles = average * x
-        let tr = document.createElement("tr");
+        let tr = document.createElement("tr"); 
         let tdNumber = document.createElement("td");
-        
         let tdAngle = document.createElement("td");
+        
         tdNumber.style.color = colors;
         tdNumber.innerText = data[x];
-        tdAngle = Math.round(trueAngles) + "°";
+        tdAngle.innerText = Math.round(trueAngles) + "°";
+
         pieTable.append(tbody);
-        tbody.append(tr)
+        
+        tbody.append(tr);
         tr.append(tdNumber);
         tr.append(tdAngle);
-        
     }
+    
+        
     
 }
 window.addEventListener("DOMContentLoaded", init);
